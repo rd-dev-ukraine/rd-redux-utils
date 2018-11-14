e70941e39dcbe98a9f181eb796b42be878fbe91c
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2018-11-14 16:11:42 +0200

    Tests

--- tests/defineAction.test.ts
+++ tests/defineAction.test.ts
@@ -2,6 +2,7 @@ import "mocha";
 import "should";
 
 import { defineAction } from "../src";
+import { Action } from "redux";
 
 describe("defineAction", () => {
     it("should create action with specified field", () => {
@@ -22,5 +23,27 @@ describe("defineAction", () => {
             value: 1111,
             type: "broken action type"
         }).should.be.eql({ type: "RD : TEST ACTION", data: "abc", value: 1111 });
+    });
+
+    it("should shrink action of own type", () => {
+        const testAction = defineAction<{ data: string; value: number }>("test action");
+
+        const instance: Action = testAction({ data: "abc", value: 111 });
+
+        testAction.is(instance).should.be.true();
+    });
+
+    it("should not shrink action of other type", () => {
+        const testAction = defineAction<{ data: string; value: number }>("test action");
+
+        const instance: Action = { type: "SOME ACTION TYPE" };
+
+        testAction.is(instance).should.be.false();
+    });
+
+    it("should not shrink nullish instance", () => {
+        const testAction = defineAction<{ data: string; value: number }>("test action");
+
+        testAction.is(undefined).should.be.false();
     });
 });
